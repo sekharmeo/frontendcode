94edd8ba85efb65215a22b307a67a74f7d1fd437
sekharmeo <mandalipodcast@gmail.com>
2025-03-21 11:50:19 +0530

    Update logout.js

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -1,6 +1,8 @@
 // src/utils/logout.js
+// src/utils/logout.js
 export const logout = () => {
     localStorage.clear(); // Remove all stored user data
     sessionStorage.clear(); // Remove session data if used
   };
+  
   
